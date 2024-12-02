5b1b5897a3779d51ef579402cdfb4cdf3740fd0e
Matt Fraser <mjffraser@hotmail.com>
2024-12-02 15:41:00 -0700

    public p&g gen using RFC3526 vals
    Random p generation isnt done yet

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,30 @@
-#include "readArgs/readArgs.hpp"
 #include "dhParams.hpp"
-#include "dhParamGen/internal/vettedPrimes.hpp"
+#include "readArgs/readArgs.hpp"
+#include "dhParamGen/public.hpp"
 
 int main(int argc, char* argv[]) {
 	//read args, init general & network fields
 	if (dh::parseArgs(argc, argv) == 1)
 		return 1;
 
+	dh::DHParams& params = dh::DHParams::get();
 	//if server, pick p & g, generate a
-	
-	cpp_int a;
-	a = cpp_int(dh::cleanString(dh::P_6144_BITS));
-	std::cout << a.backend().size() * sizeof(boost::multiprecision::limb_type) * 8 << std::endl;
+	if (params.isServer()) {
+		auto[p, g] = dh::selectPublicDHParams();			
+		
+	} 
 
-	//establish socket as server/client
-	
-	
-	//if client, recieve p & g 
-	
+	//if client, recieve p & g, generate a, send A
+	else {
 
-	//if client, generate a 
+	}
 	
-
-	//establish key & send message
-
 	return 0;
 }
+
+
+
+
+	// cpp_int a;
+	// a = cpp_int(dh::cleanString(dh::P_6144_BITS));
+	// std::cout << a.backend().size() * sizeof(boost::multiprecision::limb_type) * 8 << std::endl;

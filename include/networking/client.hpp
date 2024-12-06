bb6f5775fb2962b519607c0eeaa216f5eb698709
Matt Fraser <mjffraser@hotmail.com>
2024-12-06 15:42:19 -0700

    outline for client/server communication

---
+++ include/networking/client.hpp
@@ -0,0 +1,74 @@
+#pragma once
+
+#include "dh_params.hpp"
+
+namespace dh {
+
+/*
+ * create_client
+ *
+ * Creates client socket and returns the socket descriptor
+ */
+int create_client();
+
+/*
+ * recv_dh_pub
+ *
+ * Contacts the server and gets the public p&g values
+ * used for the key establishment.
+ *
+ * Returns a negative value on failure.
+ */
+int recv_dh_pub(cpp_int& p, cpp_int& g);
+
+/*
+ * send_A
+ *
+ * Sends g^a to the server.
+ *
+ * Returns a negative value on failure.
+ */
+int send_A(cpp_int& A);
+
+/*
+ * recv_B
+ *
+ * Recieves g^b from the server. 
+ *
+ * This function expects that the server just recieved A, 
+ * and is calculating the AES key, which is slow. An extra
+ * timeout delay is inserted while waiting to recieve B.
+ *
+ * Returns a negative value on failure.
+ */
+int recv_B(cpp_int& B);
+
+/*
+ * send_encrypted_message
+ *
+ * Sends the AES encrypted message to the server.
+ *
+ * Expects:
+ * - message: the encrypted data, as bytes.
+ * - messsage_len: the number of bytes in message.
+ * - tag: the 16-byte tag for authentication on the other side.
+ * - iv: the 16-byte iv used for encryption. 
+ *
+ * Returns a negative value on failure.
+ */
+int send_encrypted_message(unsigned char* message, 
+													 int message_len, 
+													 unsigned char* tag, 
+													 unsigned char* iv);
+
+}
+
+/* 1) Create socket.
+ * 2) Get p&g. 
+ *		2.1) Calculate a, A.
+ * 3) Send A 
+ * 4) Recieve B
+ *		4.1) Calculate DH_key
+ *		4.2) Compute AES key (SLOW)
+ * 5) Send encrypted message
+ */
---
+++ include/networking/server.hpp
@@ -0,0 +1,64 @@
+#pragma once
+
+#include "dh_params.hpp"
+
+namespace dh {
+
+/*
+ * create_server
+ *
+ * Creates server socket and returns the socket descriptor
+ */
+int create_server();
+
+/*
+ * accept_client
+ *
+ * Returns the socket descriptor for an incoming client connection.
+ */
+int accept_client();
+
+/* 
+ * establish_DH_key
+ *
+ * Engages in a series of messages with the client to establish
+ * a shared key for the AES encryption.
+ *
+ * Returns: 
+ * - a negative result on failure, the client socket should be terminated in this case.
+ * - the shared key as a cpp_int otherwise.
+ */
+cpp_int establish_DH_key();
+
+/*
+ * recv_encrypted_message
+ *
+ * Recieves the AES encrypted message and tag from the client,
+ * decrypts it, and writes to the provided plaintext buffer. 
+ *
+ * This function expects that the client just recieved B, and is
+ * calculating the AES key, which is slow. An extra timeout delay
+ * is inserted while waiting to recieve the message.
+ *
+ * Expects:
+ * - buffer: a variable size buffer to write the message to.
+ * - tag: a 16-byte container to store the tag for the encrypted message.
+ * - iv: a 16-byte contaner to store the iv used for the encryption.
+ *
+ * Returns a negative result on failure.
+ */
+int recv_encrypted_message(std::vector<unsigned char>& buffer, 
+													 unsigned char* tag, 
+													 unsigned char* iv);
+
+}
+
+/* 0) Calculate p, g, b, B
+ * 1) Create socket. 
+ * 2) Recieve client, send client p&g.
+ * 3) Recieve A from the client.
+ *		3.1) Calculate DH_key
+ *		3.2) Compute AES key (SLOW)
+ * 4) Send B
+ * 5) Recieve encrypted message.
+ */
